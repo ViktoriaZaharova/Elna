41b63de8fce60c5302a7081a052375ff4b7d382d
Viktoria <zaharova181114@gmail.com>
2022-05-28 00:53:30 +0300

    home pc

--- app/js/common.js
+++ app/js/common.js
@@ -7,6 +7,22 @@ $('.home-slider').slick({
     nextArrow: '<button type="button" class="slick-next"></button>'
 });
 
+$('.about-slider').slick({
+    slidesToShow: 1,
+    fade: true,
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>'
+});
+
+$('.partners-slider').slick({
+    variableWidth: true,
+    slidesToShow: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
+
 
 $(window).on("load", function () {
     $(".mCustomScrollbar-wrap").mCustomScrollbar({
@@ -16,8 +32,55 @@ $(window).on("load", function () {
     });
 });
 
-
 $(document).on("click", '.dropdown-menu', function (e) {
     e.stopPropagation();
 });
 
+$('.btn-add-favorite').on('click', function (e) {
+    e.preventDefault();
+    $(this).toggleClass('active');
+});
+
+$(document).ready(function () {
+
+    $(".menu-navigation__item").hover(function () {
+        let id = $(this).attr('data-tab'),
+            content = $('.js-tab-content[data-tab="' + id + '"]');
+
+        $('.btn-close-submenu').css('display', 'flex');
+
+        $('.overlay-bg').fadeIn();
+
+        $('.js-tab-trigger').removeClass('active'); // 1
+        $(this).addClass('active'); // 2
+
+        $('.js-tab-content').removeClass('active'); // 3
+        content.addClass('active'); // 4
+    });
+
+
+    $(".menu-navigation-submenu .js-tab-trigger").hover(function () {
+        let id = $(this).attr('data-tab'),
+            content = $('.js-tab-content[data-tab="' + id + '"]');
+
+
+        $('.menu-navigation-submenu .js-tab-trigger').removeClass('active'); // 1
+        $(this).addClass('active'); // 2
+
+        $('.dropdown-submenu-level').removeClass('active'); // 3
+        content.addClass('active'); // 4
+    });
+
+
+    $('.btn-close-submenu, .overlay-bg').on('click', function (e) {
+        e.preventDefault();
+        $('.js-tab-content').removeClass('active');
+        $('.js-tab-trigger').removeClass('active');
+        $('.btn-close-submenu').css('display', 'none');
+        $('.overlay-bg').fadeOut();
+    });
+
+});
+
+
+$('[name="phone"]').mask('+7 (999) 999 - 99 - 99');
